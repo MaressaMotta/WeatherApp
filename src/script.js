15aea289710406b3cd227f54068be06ffbe10c45
MaressaMotta <91914924+MaressaMotta@users.noreply.github.com>
2021-11-21 19:39:16 +0000

    js active temperature

--- src/script.js
+++ src/script.js
@@ -117,12 +117,16 @@ function getCurrentLocation(event) {
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let fahrenheitTemperature = (0 * 9) / 5 + 32;
-
+  // remove the active class the celsius link
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
